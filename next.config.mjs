616a9b8bc0f86eb787a5949dd3cb24cf375f00ca
Voodoo777 <mario@grupoquantum.uy>
2026-04-29 01:05:54 -0500

    chore: Migration to Firebase App Hosting and T-Shirt Customizer

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  }
+    typescript: {
+          ignoreBuildErrors: true,
+    }
 };
 
 export default nextConfig;
